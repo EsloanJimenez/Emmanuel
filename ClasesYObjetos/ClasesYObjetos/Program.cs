191f70cc1dbf4280203a5f2dadbcf7a339efa2aa
ejpservice <ejptheservice@gmail.com>
2024-05-25 12:37:12 -0400

    primer commit

---
+++ ClasesYObjetos/ClasesYObjetos/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+    1. Clases y Objetos :
+    - Ejercicio : Crea una clase Persona que tenga atributos nombre, edad y dirección.
+    Incluye un método para mostrar la información de la persona en consola.
+
+    Objetivo : Comprender la creación de clases y objetos.
+*/
+
