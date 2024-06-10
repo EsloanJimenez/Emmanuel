70c72e8a1be56bbd2c3c3cf2368b6a696806c061
ejpservice <ejptheservice@gmail.com>
2024-06-10 18:04:08 -0400

    Queue y alguno de sus metodos

---
+++ ColeccionQueue/ColeccionQueue/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace ColeccionList
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(int id, string fn, string ln, int age)
+        {
+            Id = id;
+            FirstName = fn;
+            LastName = ln;
+            Age = age;
+        }
+
+        public void ViewInfo()
+        {
+            Console.WriteLine($"""
+                ID:         {Id},
+                Nombre:     {FirstName},
+                Apellido:   {LastName},
+                Edad:       {Age}
+                """);
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ ColeccionQueue/ColeccionQueue/Program.cs
@@ -0,0 +1,37 @@
+﻿using ColeccionList;
+
+Queue<Person> personas = new Queue<Person>();
+
+//PARA AGREGAR UN ELEMENTO
+personas.Enqueue(new Person(1, "Enrique", "Pimentel", 30));
+personas.Enqueue(new Person(2, "Yhennifer", "Abreu", 29));
+personas.Enqueue(new Person(3, "Enyher", "Jimenez", 8));
+
+foreach (Person p in personas)
+{
+    Console.WriteLine($"""
+                ID:         {p.Id},
+                Nombre:     {p.FirstName},
+                Apellido:   {p.LastName},
+                Edad:       {p.Age}
+                """);
+    Console.WriteLine();
+}
+
+//PARA ELIMINAR ELEMENTO
+personas.Dequeue();
+
+Console.WriteLine("Despues de aver eliminando un elemento");
+
+foreach (Person p in personas)
+{
+    Console.WriteLine($"""
+                ID:         {p.Id},
+                Nombre:     {p.FirstName},
+                Apellido:   {p.LastName},
+                Edad:       {p.Age}
+                """);
+    Console.WriteLine();
+}
+
+//https://learn.microsoft.com/es-es/dotnet/api/system.collections.generic.queue-1?view=net-8.0
